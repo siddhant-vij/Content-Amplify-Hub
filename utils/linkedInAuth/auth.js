adcb4179ee38f6ee011ec7b0594bef5d966230dc
Siddhant Vij <siddhantvij201@gmail.com>
2024-06-05 15:11:12 +0530

    refactor: reAuth to fix server & browser play

--- utils/linkedInAuth/auth.js
+++ utils/linkedInAuth/auth.js
@@ -15,15 +15,20 @@ const responseType = "code";
 const state = Math.random();
 const scope = "openid profile email w_member_social";
 
-let server;
+let serverResolve;
+const serverPromise = new Promise((resolve) => {
+  serverResolve = resolve;
+});
 
-const app = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
+  console.log("Inside server");
   const reqPathname = url.parse(req.url, true).pathname;
   const reqQuery = url.parse(req.url, true).query;
 
   const redirectUriPathname = new URL(redirectUri).pathname;
 
-  if (reqPathname == "/") {
+  if (reqPathname === "/") {
+    console.log("Redirected to: ", reqPathname);
     const authUrl =
       authBaseUrl +
       "?response_type=" +
@@ -38,7 +43,7 @@ const app = http.createServer((req, res) => {
       encodeURIComponent(scope);
     res.writeHead(302, { Location: authUrl });
     res.end();
-  } else if (reqPathname == redirectUriPathname) {
+  } else if (reqPathname === redirectUriPathname) {
     console.log("Redirected to: ", redirectUriPathname);
     const reqCode = reqQuery.code;
 
@@ -60,12 +65,12 @@ const app = http.createServer((req, res) => {
     const hostname = "www.linkedin.com";
     const path = "/oauth/v2/accessToken?" + pathQuery;
     const headers = {
-      "Content-Type": "x-www-form-urlencoded",
+      "Content-Type": "application/x-www-form-urlencoded",
     };
     const body = "";
     _request(method, hostname, path, headers, body)
       .then(async (r) => {
-        if (r.status == 200) {
+        if (r.status === 200) {
           const accessToken = JSON.parse(r.body).access_token;
 
           await updateGitHubSecret(accessToken);
@@ -73,66 +78,73 @@ const app = http.createServer((req, res) => {
           res.writeHead(200, { "content-type": "text/html" });
           res.write("Access token retrieved. You can close this page");
           console.log("Access token retrieved.");
-          res.end(() => {
-            console.log("Server is closing after successful token retrieval.");
-            server.close();
-          });
+          res.end();
+          serverResolve();
         } else {
           console.error(
             "Auth. Response Error - " + r.status + JSON.stringify(r.body)
           );
           res.writeHead(r.status, { "content-type": "text/html" });
           res.write(r.status + " Internal Server Error");
-          res.end(() => {
-            console.log("Server is closing after response error.");
-            server.close();
-          });
+          res.end();
+          serverResolve();
         }
       })
       .catch((e) => {
         console.error("Auth. Request Error - " + e);
         res.writeHead(500, { "content-type": "text/html" });
         res.write("500 Internal Server Error");
-        res.end(() => {
-          console.log("Server is closing after request error.");
-          server.close();
-        });
+        res.end();
+        serverResolve();
       });
   } else {
     server.close();
   }
 });
 
-server = app.listen(3000);
+server.listen(3000);
 server.on("error", (e) => console.log("Error on port " + 3000 + " - " + e));
 server.on("listening", async () => {
   console.log("Listening on port " + 3000);
 
+  console.log("Running Puppeteer...");
+  await runPuppeteer();
+  console.log("After running Puppeteer...");
+
+  await serverPromise;
+  console.log("After serverPromise...");
+  server.close();
+  console.log("Server closed");
+});
+
+const runPuppeteer = async () => {
+  console.log("Launching Puppeteer...");
   const browser = await puppeteer.launch({ headless: true });
+  console.log("After launching Puppeteer...");
   const page = await browser.newPage();
+  console.log("After newPage...");
   await page.goto("http://127.0.0.1:3000");
+  console.log("After goto at port 3000...");
 
   await page.waitForSelector("#username");
+  console.log("After waitForSelector #username...");
   await page.type("#username", linkedInUsername);
+  console.log("After type #username...");
 
   await page.waitForSelector("#password");
+  console.log("After waitForSelector #password...");
   await page.type("#password", linkedInPassword);
+  console.log("After type #password...");
 
   await Promise.all([
     page.waitForNavigation(),
     page.click(".btn__primary--large"),
   ]);
-
-  const serverResponse = new Promise((resolve) => {
-    server.on("close", () => {
-      console.log("Server closed, proceeding to close browser");
-      resolve();
-    });
-  });
-  await serverResponse;
+  console.log("After click .btn__primary--large...");
 
   await browser.close();
-});
+  console.log("Puppeteer closed");
+};
 
 // https request wrapper
 const _request = (method, hostname, path, headers, body) => {
